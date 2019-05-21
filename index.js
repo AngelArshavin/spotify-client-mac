c3584a231b3418bcf0f75d44ac7e4e2fc4abb3d3
AngelArshavin <angel.arshavin@outlook.com>
2019-05-21 09:22:05 -0700

    Added player option, help and get lyrics commands

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const _ = require('lodash');
 const prompt = require('prompt');
 const parseSearchResults = require('./parsers/');
 const printSearchResults = require('./printers/');
+const spotifyClient = require('./osascripts/');
+
+// need client access token for genius
+let lyricist = require('lyricist');
+if(process.env['GeniusClientAccessToken']){
+	lyricist = lyricist(process.env['GeniusClientAccessToken']);
+}
 
 const spotifyApi = new spotify();
 
@@ -48,9 +55,12 @@ const SearchOptions = {
 program
 	.version('0.0.1')
 	.command('search <type> [query...]')
+	.description('Search for a <track (t) | artist (ar) | album (al) | playlist (p) > (searches tracks by default)')
+	.alias('s')
 	.action((type, query) => {
 		// search for tracks by default
 		var searchFn = SearchOptions['track'].fn;
+		console.log(type, query)
 		var searchQry = `${type} ${query.join(' ')}`;
 		var resultType = SearchOptions['track'].type;
 
@@ -60,21 +70,211 @@ program
 			resultType = SearchOptions[type].type;
 		}
 
-		searchFn(searchQry)
-			.then(function(data) {
-				var results = parseSearchResults(resultType, data);
-				printSearchResults(resultType, results);
-				prompt.start();
-				prompt.get(['selection'], function (err, result) {
+		searchFn(searchQry).then((data) => {
+			var results = parseSearchResults(resultType, data);
+			printSearchResults(resultType, results);
+			prompt.start();
+			prompt.get(['selection'], function (err, result) {
+				if(results[result.selection-1]){
 					var selectedSpotifyURI = results[result.selection-1].spotifyURI;
-					osascript.execute('tell application "Spotify" to play track uri', { uri : selectedSpotifyURI },function(err, result, raw){
-						  if (err) return console.error(err)
-						  console.log(result, raw)
-					})
-				})
-			}, function(err) {
-				console.error(err);
+					spotifyClient.play(selectedSpotifyURI).then((result) => {
+						console.log(result);
+					});
+				}
 			});
+		});
 	});
+
+program
+	.command('info')
+	.alias('i')
+	.description('Display information about the current track along with player status')
+	.action(() => {
+		spotifyClient.status().then((result) => {
+			console.log(result);
+		})
+	})
+
+program
+	.command('play [uri]')
+	.description('Continue playing current track or play the track with the provided URI')
+	.action((uri) => {
+		if(uri){
+			spotifyClient.play(uri);
+		} else {
+			spotifyClient.play();
+		}
+	})
+
+
+program
+	.command('pause')
+	.description('Pause the current track')
+	.action(() => {
+		spotifyClient.pause().then((result) => {
+			console.log(result);
+		})
+	})
+
+program
+	.command('next')
+	.alias('n')
+	.description('Play the next track in the queue')
+	.action(() => {
+		spotifyClient.next().then((result) => {
+			console.log(result);
+		})
+	})
+
+program
+	.command('back')
+	.alias('b')
+	.description('Play the previous track')
+	.action(() => {
+		spotifyClient.previous().then((result) => {
+			console.log(result);
+		})
+	})
+
+program
+	.command('mute')
+	.alias('m')
+	.description('Mute player')
+	.action(() => {
+		spotifyClient.mute().then((result) => {
+			console.log(result);
+		})
+	})
+
+program
+	.command('unmute')
+	.alias('u')
+	.description('Unmute player')
+	.action(() => {
+		spotifyClient.unmute().then((result) => {
+			console.log(result);
+		})
+	})
+
+program
+	.command('volume')
+	.alias('v')
+	.description('Display player volume')
+	.action(() => {
+		spotifyClient.getVolume().then((result) => {
+			console.log(result);
+		})
+	})
+
+program
+	.command('+ [deltaVolume]')
+	.description('Turn the volume up by given amount (0-100), default:10')
+	.action((deltaVolume) => {
+		var changeInVolume = deltaVolume ? deltaVolume : 10;
+		spotifyClient.setVolume(changeInVolume).then((result) => {
+			console.log(result);
+		})
+	})
+
+program
+	.command('- [deltaVolume]')
+	.description('Turn the volume down by given amount (0-100), default:10')
+	.action((deltaVolume) => {
+		var changeInVolume = deltaVolume ? -deltaVolume : -10;
+		spotifyClient.setVolume(changeInVolume).then((result) => {
+			console.log(result);
+		})
+	})
+
+program
+	.command('p')
+	.description('Toggle play/pause')
+	.action(() => {
+		spotifyClient.togglePlayPause();
+	});
+
+program
+	.command('replay')
+	.alias('r')
+	.description('Replay current track')
+	.action(() => {
+		spotifyClient.replay();
+	})
+
+program
+	.command('position [newPosition]')
+	.alias('pos')
+	.description('Get or set player position [mm:ss], e.g: pos 1:23')
+	.action((newPosition) => {
+		if(newPosition){
+			spotifyClient.setPosition(newPosition);
+		} else {
+			spotifyClient.getPosition().then((position) => {
+				console.log(position);
+			})
+		}
+	});
+
+program
+	.command('quit')
+	.alias('q')
+	.description('Quit Spotify :(')
+	.action(() =>{
+		spotifyClient.quit();
+	});
+
+program
+	.command('open')
+	.alias('o')
+	.description('Open Spotify :)')
+	.action(() =>{
+		spotifyClient.start();
+	});
+
+program
+	.command('shuffle')
+	.alias('ts')
+	.description('Toggle shuffle on/off')
+	.action(() => {
+		spotifyClient.shuffle().then((status) => {
+			console.log(status);
+		});
+	})
+
+program
+	.command('repeat')
+	.alias('tr')
+	.description('Toggle repeat on/off')
+	.action(() => {
+		spotifyClient.repeat().then((status) => {
+			console.log(status);
+		});
+	})
+
+program
+	.command('share [type]')
+	.alias('sh')
+	.description('Display share <uri|url> and copy value to clipboard')
+	.action((type) => {
+		spotifyClient.share(type);
+	})
+
+program
+	.command('lyrics')
+	.alias('ly')
+	.description('Display the lyrics of currently playing track')
+	.action((type) => {
+		spotifyClient.status().then((trackInfo) => {
+			if(!process.env['GeniusClientAccessToken']){
+				console.log('You need to set the Client Access Token for Genius API.');
+				console.log('Sign up for API access here: https://genius.com/api-clients');
+				console.log('add key to your environment variables: export GeniusClientAccessToken=YourClientAccessTokenHere')
+				return;
+			}
+			lyricist.song({search: `${trackInfo.artist} ${trackInfo.track}`}, function (err, song) {
+				console.log(song.lyrics);
+			});
+		})
+	})
 
 program.parse(process.argv);
---
+++ osascripts/index.js
@@ -0,0 +1,173 @@
+'use strict';
+
+const osascript = require('node-osascript');
+const Promise = require('bluebird');
+const execute = Promise.promisify(osascript.execute);
+const moment = require('moment');
+// custom moment plugin for formatting track duractions
+require("moment-duration-format");
+
+// coz js...
+function _copyToClipboard(data) {
+	var proc = require('child_process').spawn('pbcopy');
+	proc.stdin.write(data);
+	proc.stdin.end();
+}
+
+function _generateURLfromURI(uri){
+	var uriComponent = uri.split(':').slice(-1)[0];
+	return `http://open.spotify.com/track/${uriComponent}`;
+}
+
+function play(uri){
+	if(uri){
+		return execute('tell application "Spotify" to play track uri', {uri});
+	} else {
+		return execute('tell application "Spotify" to play');
+	}
+}
+
+function status(){
+	return Promise.props({
+		status: execute('tell application "Spotify" to player state as string'),
+		artist: execute('tell application "Spotify" to artist of current track as string'),
+		album: execute('tell application "Spotify" to album of current track as string'),
+		track: execute('tell application "Spotify" to name of current track as string'),
+		duration: execute('tell application "Spotify" to duration of current track').then((durationMs) => {
+			return moment.duration(durationMs, 'milliseconds').format();
+		}),
+		position: getPosition()
+	});
+}
+
+function pause(){
+	return execute('tell application "Spotify" to pause');
+}
+
+function next(){
+	return execute('tell application "Spotify" to next track');
+}
+
+function previous(){
+	return execute('tell application "Spotify"\n set player position to 0\n previous track\n end tell');
+}
+
+function mute(){
+	return execute('tell application "Spotify" to set sound volume to 0');
+}
+
+function unmute(){
+	return execute('tell application "Spotify" to set sound volume to 100');
+}
+
+function setVolume(deltaVolume){
+	return getVolume().then((currentVolume) => {
+		var newVolume = currentVolume + deltaVolume;
+		if(newVolume > 100) newVolume = 100;
+		if(newVolume < 0) newVolume = 0;
+		return execute('tell application "Spotify" to set sound volume to newVolume', {newVolume});
+	});
+}
+
+function getVolume(){
+	return execute('tell application "Spotify" to sound volume as integer');
+}
+
+function replay(){
+	return execute('tell application "Spotify" to player position').then((positionSecs) => {
+		if(positionSecs < 4){
+			return previous();
+		} else {
+			return setPosition(0);
+		}
+	});
+}
+
+function togglePlayPause(){
+	return status().then((status) => {
+		return execute('tell application "Spotify" to playpause');
+	});
+}
+
+function getPosition(){
+	return execute('tell application "Spotify" to player position').then((positionSecs) => {
+		var position = moment.duration(positionSecs, 'seconds').format("h:mm:ss", { forceLength: true  });
+		if(position.length < 3){
+			position = `00:${position}`;
+		}
+
+		return position;
+	});
+}
+
+function setPosition(newPosition){
+	var durationTemplate = '00:00:00'.split("");
+	// replace template characters starting from the last position
+	// with the user given duration in reverse order
+	newPosition.toString().split("").reverse().forEach((character, index) => {
+		durationTemplate[durationTemplate.length - (index + 1)]  = character;
+	});
+	var newPositionDuration = moment.duration(durationTemplate.join("")).asSeconds();
+	return execute('tell application "Spotify" to set player position to newPositionDuration', {newPositionDuration});
+}
+
+function quit(){
+	return execute('tell application "Spotify" to quit');
+}
+
+function start(){
+	return execute('tell application "Spotify" to activate');
+}
+
+function shuffle(){
+	return execute('tell application "Spotify" to set shuffling to not shuffling').then(() => {
+		return execute('tell application "Spotify" to repeating');
+	});
+}
+
+function repeat(){
+	return execute('tell application "Spotify" to set repeating to not repeating').then(() => {
+		return execute('tell application "Spotify" to repeating');
+	});
+}
+
+function share(type){
+	return execute('tell application "Spotify" to spotify url of current track').then((uri) => {
+		if(type == 'uri'){
+			_copyToClipboard(uri);
+			console.log(`${uri} copied to clipboard.`);
+			return uri;
+		} else if(type == 'url'){
+			var url = _generateURLfromURI(uri);
+			_copyToClipboard(url);
+			console.log(`${url} copied to clipboard.`);
+			return url;
+		} else {
+			var url = _generateURLfromURI(uri);
+			console.log(`SpotifyURI: ${uri}`);
+			console.log(`Spotify URL: ${url}`);
+			return { uri, url };
+		}
+	})
+}
+
+module.exports = {
+	play,
+	status,
+	pause,
+	next,
+	previous,
+	mute,
+	unmute,
+	setVolume,
+	getVolume,
+	replay,
+	togglePlayPause,
+	getPosition,
+	setPosition,
+	quit,
+	start,
+	share,
+	shuffle,
+	repeat
+}
